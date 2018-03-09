ebc1072ebb9654d03b3e146906fa12f4d27b94c2
ozamanan <manan.oza0001@gmail.com>
2018-03-09 07:54:59 +0530

    test

--- dcgan.py
+++ dcgan.py
@@ -205,7 +205,7 @@ class MNIST_DCGAN(object):
 if __name__ == '__main__':
     mnist_dcgan = MNIST_DCGAN()
     timer = ElapsedTimer()
-    mnist_dcgan.train(train_steps=10000, batch_size=256, save_interval=500)
+    mnist_dcgan.train(train_steps=10000000, batch_size=256, save_interval=500)
     timer.elapsed_time()
     mnist_dcgan.plot_images(fake=True)
     mnist_dcgan.plot_images(fake=False, save2file=True)
